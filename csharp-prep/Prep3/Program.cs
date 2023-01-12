bf9e1e5c74d7745ba5d6f67b82a3983e0e4c6a71
BenjaminDowdle <benjamin.dowdle99@gmail.com>
2023-01-11 20:46:57 -0700

    Prep 3, Prep 4, and Prep 5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain = "yes";
+
+        while (playAgain == "yes")
+        {
+
+
+
+            Random randomGen = new Random();
+            int magicNumber = randomGen.Next(1, 101);
+
+            int guess = -1;
+
+            while (guess != magicNumber)
+            {
+                Console.WriteLine("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+            }
+            Console.WriteLine("Would you like to play again? ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
