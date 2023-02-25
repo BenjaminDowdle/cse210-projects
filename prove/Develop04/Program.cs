f096a88b0c88d403dc633682a057e3be17aac88a
BenjaminDowdle <benjamin.dowdle99@gmail.com>
2023-02-25 00:10:05 -0700

    Completed Mindfullness App

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,77 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+
+        string choice = "";
+
+        Console.Clear();
+        Console.WriteLine("Hello! Welcome to MindFull.");
+
+        // --------------------Menu-----------------------
+
+        while (choice != "4")
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflecting Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Quit");
+            Console.Write("Please make a selection (1-4): ");
+
+            choice = Console.ReadLine();
+            int activityInt = Int32.Parse(choice);
+
+            // --------------Breathing Activity----------------
+            if (choice == "1")
+            {
+                BreathingActivity breathingActivity = new BreathingActivity();
+
+                breathingActivity.ActivityName = "Breathing Activity";
+                breathingActivity.displayWelcomeMessage();
+                breathingActivity.displayDescription(activityInt);
+
+                breathingActivity.Time = Int32.Parse(Console.ReadLine());
+
+                breathingActivity.GetReady();
+                breathingActivity.displayBreathing(breathingActivity.Time);
+            }
+
+            // --------------Reflecting Activity----------------
+            else if (choice == "2")
+            {
+                ReflectingActivity reflectingActivity = new ReflectingActivity();
+
+                reflectingActivity.ActivityName = "Reflecting Activity";
+                reflectingActivity.displayWelcomeMessage();
+                reflectingActivity.displayDescription(activityInt);
+
+                reflectingActivity.Time = Int32.Parse(Console.ReadLine());
+
+                reflectingActivity.GetReady();
+                reflectingActivity.displayReflection(reflectingActivity.Time);
+            }
+
+            // --------------Listing Activity----------------
+            else if (choice == "3")
+            {
+                ListingActivity listingActivity = new ListingActivity();
+
+                listingActivity.ActivityName = "Listing Activity";
+                listingActivity.displayWelcomeMessage();
+                listingActivity.displayDescription(activityInt);
+
+                listingActivity.Time = Int32.Parse(Console.ReadLine());
+
+                listingActivity.GetReady();
+                listingActivity.displayListing(listingActivity.Time);
+            }
+        }
+
+
+        // --------------Goodbye----------------
+        Console.Clear();
+        Console.WriteLine("Thanks for using the app. Goodbye.");
+
     }
 }
